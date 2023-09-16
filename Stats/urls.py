4643efe3d96f81c76ca27c6d37bfb84c46e820bd
kenier <kenierR1978@gmail.com>
2023-09-15 20:48:06 -0500

    add static and css

--- Stats/urls.py
+++ Stats/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path,include
 from Stats import views
 #direccion es del proyecto
+
+app_name = 'Stats'
 urlpatterns = [
     path('index/',views.index,name='index')
 ]
--- mlbproject/settings.py
+++ mlbproject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',    
-    'Stats.apps.StatsConfig',
+    'Stats',
 ]
 
 MIDDLEWARE = [
