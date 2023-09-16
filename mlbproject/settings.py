b4ff3f1953567508b4eee6303204e10d34075652
kenier <kenierR1978@gmail.com>
2023-09-16 02:20:27 -0500

    db arreglos

--- mlbproject/settings.py
+++ mlbproject/settings.py
@@ -89,23 +89,13 @@ WSGI_APPLICATION = 'mlbproject.wsgi.application'
 #    }
 #}
 
-
-if DEBUG:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost:5432/mysite',
-        )
-
-    }
-
-
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://kenier:slippery@localhost:5432/mlb',
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
