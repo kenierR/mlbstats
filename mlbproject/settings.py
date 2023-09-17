7efeb97a4b932c68a7cb948657df0e35ebc83be1
kenier <kenierR1978@gmail.com>
2023-09-16 19:23:41 -0500

    all bingin here

--- mlbproject/settings.py
+++ mlbproject/settings.py
@@ -25,17 +25,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='slippery')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
-print("-----------------------*****----------------------------------",DEBUG)
 
+DEBUG = True #'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["*"]
-print("-------------host",ALLOWED_HOSTS)
+ALLOWED_HOSTS = ['*']
+
 # RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 # if RENDER_EXTERNAL_HOSTNAME:
-#     print("------------------------adding host",ALLOWED_HOSTS)
+#     print(RENDER_EXTERNAL_HOSTNAME)
 #     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
