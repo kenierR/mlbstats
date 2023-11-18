37bda5ae75e1639e27e368d479b98101533b899c
kenier <kenierR1978@gmail.com>
2023-11-18 10:34:03 -0600

    add enviroment variables

--- Crypto/source/cb.py
+++ Crypto/source/cb.py
@@ -1,10 +1,10 @@
 from coinbase.wallet.client import Client
-
+import os
 
 class CB:
     def __init__(self):
-        self.coinbase_API_key = "Cqalyyr0Eu32dpwL"
-        self.coinbase_API_secret = "NZgXD5mVnWa1e2e9Eum8quDocb4L8nuF"
+        self.coinbase_API_key = os.environ.get('coinbase_API_key')
+        self.coinbase_API_secret = os.environ.get('coinbase_API_secret')
         self.client = Client(self.coinbase_API_key, self.coinbase_API_secret)
 
     def spot(self):
@@ -15,8 +15,10 @@ class CB:
     def btc_balance_inc(self,precio_comp,inc):
         balance = float(self.client.get_primary_account()['balance']['amount'])*float(precio_comp)
         esperado_valor = balance+float(inc)
-        print('balance:',balance,'valor esperado:',esperado_valor,'satohis:',self.client.get_primary_account()['balance']['amount'])
+        #print('balance:',balance,'valor esperado:',esperado_valor,'satohis:',self.client.get_primary_account()['balance']['amount'])
         return esperado_valor//float(self.client.get_primary_account()['balance']['amount'])
     def btc_balance(self):
         balance = float(self.client.get_primary_account()['balance']['amount'])*float(self.spot())
-        return balance
+        return "{:.2f}".format(balance)
+    def usd_balance(self):
+        return "{:.2f}".format(float(self.client.get_account('USD')['balance']['amount']))
--- Crypto/views.py
+++ Crypto/views.py
@@ -12,33 +12,34 @@ btc_bal = []
 btc_to_buy = []
 def index(request):
     precio_compra = 36800
+    rango = 100
+    time_len = 200
     content = {}
     sp = CB()
     spot = sp.spot()
     #precios.insert(len(spot),spot)
-    print(spot)
     precios.append(float(spot))
     now = datetime.now()
     t = str(now.hour) + ':' + str(now.minute) + ':' + str(now.second)
     tiempo.append(t)
     top.append(float(precio_compra))
-    print(top)
 
     #obtener valance
-    btc_balance_inc = sp.btc_balance_inc(precio_compra,100)
-    btc_balance_dec = sp.btc_balance_inc(precio_compra,-100)
+    btc_balance_inc = sp.btc_balance_inc(precio_compra,rango)
+    btc_balance_dec = sp.btc_balance_inc(precio_compra,-rango)
     btc_bal.append(btc_balance_inc)
     btc_to_buy.append(btc_balance_dec)
-    print('balance:',btc_balance_inc)
     btc_balance = sp.btc_balance()
+    total_balane = float(btc_balance) + float(sp.usd_balance())
 
 
-    content['precios'] = precios[-200:]
-    content['tiempo']  = tiempo[-200:]
-    content['top'] = top[-200:]
-    content['btc_balance_inc'] = btc_bal[-200:]
-    content['btc_balance'] = float(btc_balance)
-    content['btc_to_buy'] = btc_to_buy[-200:]
-
+    content['precios'] = precios[-time_len:]
+    content['tiempo']  = tiempo[-time_len:]
+    content['top'] = top[-time_len:]
+    content['btc_balance_inc'] = btc_bal[-time_len:]
+    content['btc_balance'] = btc_balance
+    content['btc_to_buy'] = btc_to_buy[-time_len:]
+    content['usd_balance'] = sp.usd_balance()
+    content['total_balance'] = total_balane
     #print(content['precios'])
     return render(request,'Crypto/index.html',content)
